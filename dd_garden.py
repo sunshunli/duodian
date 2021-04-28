6643eaf065578c6cb6b20861bba4f8645e4a8935
sunshunli <sunshunli@users.noreply.github.com>
2021-04-28 11:25:14 +0800

    修复领取水滴，水桶超过500时无法领取问题

--- dd_garden.py
+++ dd_garden.py
@@ -20,10 +20,10 @@ from urllib.parse import urlparse
 # pipenv install -r requirements.txt
 ###################################################
 # 对应方案2: 下载到本地,需要此处填写
-cookies1 = ""
+cookies1 = "tempid=C957C764BD50000210E4171010C91D36; updateTime=1619429326000; device=HUAWEI%20HUAWEI%20YAL-AL00%20LMY47I; sysVersion=Android-5.1.1; screen=1280*720; apiVersion=5.0.4; dSource=; oaid=; tdc=; utmId=; androidId=70c1430d8f180e33; channelId=dm010000000001; currentTime=1619534618614; abFlag=1-1-B; lastInstallTime=1619534455218; version=5.0.4; tpc=; firstInstallTime=1619534455218; networkType=1; deliveryLng=116.410277; deliveryLat=39.916501; cid=160a3797c8a8315f5aa; sessionId=876d9892c72244c8854ef2ca349f1c25; User-Agent=dmall/5.0.4%20Dalvik/2.1.0%20%28Linux%3B%20U%3B%20Android%205.1.1%3B%20YAL-AL00%20Build/LMY47I%29; xyz=ac; appName=com.wm.dmall; smartLoading=1; utmSource=; wifiState=1; gatewayCache=; isOpenNotification=1; inited=true; console_mode=0; uuid=70c1430d8f180e33; store_id=150; storeId=150; vender_id=1; venderId=1; businessCode=1681; appMode=online; storeGroupKey=6d6728171e7121fa5c838248af04be9b@MS0xNTAtMQ; platformStoreGroupKey=43913b6f8c3125d3f87b46aaec7616d4@MjAzMi04MDA4Mg; originBusinessFormat=1-2-4-8; appVersion=5.0.4; platform=ANDROID; dmall-locale=zh_CN; token=ca23a209-c415-4e64-bbd3-5d03730b67e9; ticketName=A514D41153123DCAE3DB465DAFE0FE1F4D4116123140B978113F668820EA821CDE306E5BA281687C853D43129DE1AC99FBBB3E48022897B31562185781D4EA342116FCDA4BA78559A415AF31A5FD26DB3BFA99EE0B382DA8733E1163A56D07A4860FC9A0B4716491E41E4FD157963152272396876117C4A3AA157C9E9C3A7EC9; userId=395388955; lat=39.916501; lng=116.410277; addr=%E5%8C%97%E4%BA%AC%E5%B8%82%E4%B8%9C%E5%9F%8E%E5%8C%BA%E5%88%A9%E7%94%9F%E5%86%99%E5%AD%97%E6%A5%BC; community=%E5%88%A9%E7%94%9F%E5%86%99%E5%AD%97%E6%A5%BC; areaId=110101; session_id=876d9892c72244c8854ef2ca349f1c25; env=app; first_session_time=1619534530736; session_count=2; recommend=1; dmTenantId=1; risk=1"
 cookies2 = ""
 
-cookiesList = [cookies1, cookies2]   # 多账号准备
+cookiesList = [cookies1, ]   # 多账号准备
 
 # 通知服务
 BARK = ''                   # bark服务,自行搜索; secrets可填;形如jfjqxDx3xxxxxxxxSaK的字符串
@@ -55,13 +55,12 @@ if "DD_GARDEN_COOKIE" in os.environ:
         TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
         TG_USER_ID = os.environ["TG_USER_ID"]
         print("Telegram 推送打开")
-
-
 ###################################################
 # 可选项
 UserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; LYA-AL10 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36 Dmall/5.0.4"
 ts = datetime.now() - timedelta(hours=1)
 previous_time_stamp = int(time.mktime(ts.timetuple()) * 1000)
+
 
 def get_time():
     mins = int(time.time())
@@ -243,8 +242,7 @@ def get_task_list(cookies, msg):
 
 def do_task(cookies, task):
     # 做任务集水滴
-    print(f"开始执行{task['taskName']} - {task['taskDesc']}")
-
+    # print(f"开始执行{task['taskName']} - {task['taskDesc']}")
     # 判断任务的类型，决定如何完成任务
     switch = {'1': do_task_type1,  # 注意此处不要加括号
               '2': do_task_type2,
@@ -284,10 +282,19 @@ def get_daily_reward(cookies, task):
     except Exception as e:
         print(e)
         return
-    data = response.json()["data"]
-    if data['haveTimeInterval'] is not None:
-        # 需要定时器
-        fun_timer(data['timeInterval'], get_daily_reward, [cookies, task])
+    data = response.json()
+    if data['code'] == 'USER_3008':
+        print(data['message'], '开始执行10次浇水任务')
+        i = 0
+        while i < 10:
+            time.sleep(5)
+            water_tree(cookies)
+            i = i + 1
+        get_daily_reward(cookies, task)
+    else:
+        if data['data']['haveTimeInterval'] is not None:
+            # 需要定时器
+            fun_timer(data['timeInterval'], get_daily_reward, [cookies, task])
 
 
 def do_task_type1(cookies, task):
@@ -348,7 +355,8 @@ def do_task_type2(cookies, task):
         return
     result = response.json()
     if result:
-        fun_timer(task['timeInterval'], finish_browser_page, [cookies, task])
+        # 写死创建新线程，等待15s后执行浏览确认任务
+        fun_timer(15, finish_browser_page, [cookies, task])
 
 
 def finish_browser_page(cookies, task):
@@ -393,11 +401,11 @@ def finish_browser_page(cookies, task):
         print("网络请求异常,finish_browser_page")
         return
     result = response.json()
-    print(response)
+    print('浏览任务1', result)
 
 
 def finish_browser_task(cookies, task):
-    print(f"开始执行{task['taskName']}")
+    # print(f"开始执行{task['taskName']}")
     # 浏览商品
     print('执行浏览商品任务')
     headers = {
@@ -430,7 +438,7 @@ def finish_browser_task(cookies, task):
         return
     data = response.json()
 
-    print(f'执行每日签到任务，{data["message"]}')
+    print('浏览任务1', data)
 
 
 def do_task_type5(cookies, task):
