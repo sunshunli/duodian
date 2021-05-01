42e395e935cfe64a0ae0c35ce168cf02a8a7a9b0
sunshunli <sunshunli@users.noreply.github.com>
2021-05-01 23:23:32 +0800

    fix

--- bin/dd_garden.py
+++ bin/dd_garden.py
@@ -120,8 +120,8 @@ def touch_tree_drop(cookies):
         print(f"点击果树掉落{data.get('config').get('awardNums')}水滴")
         # 拾取掉落的水滴
         receive_tree_drop(cookies, str(data.get('id')))
-    elif data.get('awardType', -1) == 3:
-        print(f"点击果树掉落{data['awardName']}")
+    elif data.get('config').get('awardType', -1) == 3:
+        print(f"点击果树掉落{data.get('config').get('awardName')}")
         # 拾取掉落的优惠券
         receive_tree_drop(cookies, str(data.get('id')))
 
